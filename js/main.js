080f3bf3fbd4bb89dd488be172204ab9ad7ebd97
Youssef <y.2003eg@gmail.com>
2023-03-21 04:44:50 +0200

    final UI

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ navMenu.onclick = function () {
 function closeMenu() {
   navMenu.classList.toggle("show-menu");
 }
+
+var dropMenu = document.querySelector(".drop-menu");
+var addBtn = document.querySelector(".add-btn");
+var closeMenuBtn = document.querySelector(".close-drop-menu");
+
+addBtn.onclick = function () {
+  showHideDropMenu();
+};
+closeMenuBtn.onclick = function () {
+  showHideDropMenu();
+};
+
+function showHideDropMenu() {
+  dropMenu.classList.toggle("show-drop-menu");
+}
