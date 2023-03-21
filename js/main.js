b6ef07bbaf6ca25cbd524273dd8b8f00e0d9f95e
Youssef <y.2003eg@gmail.com>
2023-03-21 19:54:36 +0200

    adding purchases

--- js/main.js
+++ js/main.js
@@ -1,28 +1,63 @@
-var navIcon = document.querySelector("nav i");
-var navMenu = document.querySelector(".menu");
-
-navIcon.onclick = function () {
-  closeMenu();
-};
-navMenu.onclick = function () {
-  closeMenu();
+let dataObj = {
+  purchase: "name",
+  price: 0,
 };
 
-function closeMenu() {
-  navMenu.classList.toggle("show-menu");
+var dataArr = [];
+
+if (localStorage.getItem("purchases")) {
+  dataArr = JSON.parse(localStorage.getItem("purchases"));
 }
 
-var dropMenu = document.querySelector(".drop-menu");
-var addBtn = document.querySelector(".add-btn");
-var closeMenuBtn = document.querySelector(".close-drop-menu");
+var addPurchaseBtn = document.getElementById("confirm-purchase");
+var purchaseInp = document.getElementById("purchase-name");
+var priceInp = document.getElementById("purchase-price");
 
-addBtn.onclick = function () {
-  showHideDropMenu();
-};
-closeMenuBtn.onclick = function () {
-  showHideDropMenu();
-};
+priceInp.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    addPurchaseBtn.click();
+  }
+});
 
-function showHideDropMenu() {
-  dropMenu.classList.toggle("show-drop-menu");
+addPurchaseBtn.addEventListener("click", () => {
+  dataObj.purchase = purchaseInp.value;
+  dataObj.price = priceInp.value;
+  purchaseInp.value = "";
+  priceInp.value = "";
+  addObjToArr(dataObj);
+});
+
+function addObjToArr(obj) {
+  let customObj = { ...obj };
+  dataArr.push(customObj);
+  console.log(dataArr);
+  localStorage.setItem("purchases", JSON.stringify(dataArr));
+  showData(dataArr);
 }
+
+function showData(arr) {
+  var listOfPurchases = document.querySelector("ul");
+  listOfPurchases.innerHTML = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    let purchaseLi = document.createElement("li");
+    let nameSpan = document.createElement("span");
+    let priceSpan = document.createElement("span");
+    nameSpan.classList = "item";
+    priceSpan.classList = "price";
+    nameSpan.innerHTML = arr[i].purchase;
+    priceSpan.innerHTML = arr[i].price;
+    purchaseLi.appendChild(nameSpan);
+    purchaseLi.appendChild(priceSpan);
+    listOfPurchases.appendChild(purchaseLi);
+  }
+
+  var purchasesNum = document.querySelector(".pur-num");
+  purchasesNum.innerHTML = arr.length;
+}
+
+showData(dataArr);
